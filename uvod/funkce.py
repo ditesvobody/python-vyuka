de4ba04bf224e78c522cd042e48d9224bac152e6
Peta Bujny <buiphananh@beanstudent.czgit config --global user.name Peta>
2026-04-24 09:14:48 +0200

    prvnifunkce v python

---
+++ uvod/funkce.py
@@ -0,0 +1,9 @@
+
+def umocneni():
+    cisloStr = input ("zadejte cislo")
+    cislo1 = int(cisloStr)
+    print("cislo je:" ,cislo1)
+    print("cislo na druhou je:" , cislo1 * cislo1)
+    print("cislo na treti je:", cislo1 ** 3)
+
+umocneni()
